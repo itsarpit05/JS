4fdec127942019f01800dab48f521d7ce219ab9d
itsarpit05 <101167160+itsarpit05@users.noreply.github.com>
2025-07-25 16:12:59 +0000

    operators

---
+++ basics/operations.js
@@ -0,0 +1,26 @@
+let a = 4
+let nega= -a
+console.log(nega);
+
+console.log(5**4)  // means 5^4=625
+console.log(2/3);  // it actually divides and gives accurate value = 0.6666.....
+console.log(2%3);  // quotient = 2
+
+
+let s1 = "Hi"
+let s2 = "Me"
+
+console.log(s1+" "+s2);   // Hi Me
+console.log(s1-s2);     // Prints NaN  
+
+
+console.log("1"+3);     //prints 13
+console.log(1+"4");    //    prints 14
+console.log(1+3+"4");  //prints 44
+console.log("1"+"3"+4); // prints 134 
+
+
+
+
+
+
