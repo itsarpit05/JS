ad9bba5fbbb12bcccf7a72c477dd202318ba1add
itsarpit05 <101167160+itsarpit05@users.noreply.github.com>
2025-07-25 15:17:34 +0000

    2nd change

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accntId= 1234;
+let accountPswd = 4;
+var accntEmail = "arpit@gmail.com"
+accntadrress= "Jhansi"  // default memory location without assigning 
+let accntState;  // undefined value if not assigned any value
+
+//  accntId=123;
+ console.log(accntId);
+ accountPswd=5;
+ accntEmail="qw@gmail.com"
+ accntadrress="IN"
+ /*
+ Use only const and let for variables , becuase of block scope and functional scope
+ */
+
+ console.table([accntId , accountPswd , accntEmail ,accntadrress ,accntState])
+ 
