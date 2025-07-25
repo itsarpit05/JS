3a76b1f56088fb7a2fab1762cfebb42ccf93e987
itsarpit05 <101167160+itsarpit05@users.noreply.github.com>
2025-07-25 15:26:07 +0000

    variables

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // transforms entire code in new JS form
+
+// number -> range = 2^53
+// binint
+// string 
+// boolean = true/false
+//null =>standalone value
+//undefined 
+// symbol - uniqueness(used in React)
+
+/* ---Non primtive data types ---*/
+//object 
+
+console.log(typeof null);      // type is OBJECT
+console.log(typeof undefined);   // type is undefined
